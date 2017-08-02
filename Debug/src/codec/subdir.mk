c7b9095b5d1647c2934862f40a9aeaac0fc88b2b
dikey.huang <dikey.huang@chroma.com.tw>
2017-08-02 11:07:15 +0800

    routine backup : build ok

--- Debug/src/codec/subdir.mk
+++ Debug/src/codec/subdir.mk
@@ -21,6 +21,7 @@ C_SRCS += \
 ../src/codec/MethodsBag.c \
 ../src/codec/NameTableEntries.c \
 ../src/codec/StringTable.c \
+../src/codec/QNames.c \
 ../src/codec/UCSString.c 
 
 OBJS += \
@@ -41,6 +42,7 @@ OBJS += \
 ./src/codec/MethodsBag.o \
 ./src/codec/NameTableEntries.o \
 ./src/codec/StringTable.o \
+./src/codec/QNames.o \
 ./src/codec/UCSString.o 
 
 C_DEPS += \
@@ -61,6 +63,7 @@ C_DEPS += \
 ./src/codec/MethodsBag.d \
 ./src/codec/NameTableEntries.d \
 ./src/codec/StringTable.d \
+./src/codec/QNames.d \
 ./src/codec/UCSString.d 
 
 
@@ -68,7 +71,7 @@ C_DEPS += \
 src/codec/%.o: ../src/codec/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\appHandshake" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\codec\appHandCodec" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\transport" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\service" -I"D:\Data\MCHN4240\workspace_c\OpenV2G\trunk\src\test" -O0 -g3 -pedantic -pedantic-errors -Wall -c -fmessage-length=0 -ansi -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -I"../src/codec" -I"../src/appHandshake" -I"../src/codec/appHandCodec" -I"../src/transport" -I"../src/service" -I"../src/test" -O0 -g3 -pedantic -pedantic-errors -Wall -c -fmessage-length=0 -ansi -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
